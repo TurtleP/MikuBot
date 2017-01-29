212440d933447845725cd7bdae51bcda0b8cab29
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 01:11:50 -0500

    Create sudo.rb

---
+++ commands/sudo.rb
@@ -0,0 +1,64 @@
+# sudo.rb
+
+# sets someone to Sudo
+
+
+
+$bot.command(
+
+	:sudo,
+
+	required_permissions: [:manage_roles],
+
+	min_args: 1,
+
+	description: "Set a user to Sudo status.",
+
+	usage: "sudo [user]",
+
+	help_available: true
+
+) do | event |
+
+	user = event.message.mentions.first.on(event.server)
+
+
+
+	# use do keyword to split a block across lines
+
+	sudo_status = event.server.roles.find do |role|
+
+	  role.name == "sudo"
+
+	end
+
+
+
+	#check if they *are* Sudo
+
+	if user.role? sudo_status
+
+		event << "#{user.name} is already Sudo'd!"
+
+		return
+
+	end
+
+
+
+	# set_role will wipe every other role the user has..
+
+	# not sure if that's what you intended.
+
+	user.add_role(sudo_status)
+
+
+
+	# Prefer to use ruby string interpolation instead of concatenating (:
+
+	# also, just user like you said would return a Discordrb::User object
+
+	# and not their name.
+
+	event << "#{user.mention}@mikubot~$ sudo has been activated!"
+end
