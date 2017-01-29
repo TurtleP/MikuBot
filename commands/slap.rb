634c57602d4a28fbe7090e39f1ce3c8f77d60a90
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 15:06:19 -0500

    Update slap.rb

--- commands/slap.rb
+++ commands/slap.rb
@@ -1,25 +1,14 @@
-# kick.rb
-
-# kicks a user from the server
+# slap.rb
+# slaps a user on the server
 
 $bot.command(
-
 	:slap,
-
 	min_args: 1,
-
 	description: "Slaps a user on the server.",
-
 	usage: "slap [user]",
-
 	help_available: true
-
 ) do | event, mention |
-
 	user = event.message.mentions.first.on(event.server)
-
-	event << "#{user.mention} has been slapped by #{event.message.author(event.server)}!"
-	
+	event << "#{user.mention} has been slapped by #{event.message.author()}!"
 	event << nil
-	
 end
