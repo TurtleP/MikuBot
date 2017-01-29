ad567bcb88705e3d28daefc6198f2d7a2b694f28
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 15:11:52 -0500

    Update sudo.rb

--- commands/sudo.rb
+++ commands/sudo.rb
@@ -2,46 +2,28 @@
 # sets someone to Sudo
 
 $bot.command(
-
 	:sudo,
-
 	required_permissions: [:manage_channels],
-
 	min_args: 0,
-
-	description: "Set a user to Sudo status.",
-
+	description: "Toggles a Staff user's Sudo status.",
 	usage: "sudo",
-
 	help_available: true
-
 ) do | event |
-
 	user = event.message.author()
 
 	# use do keyword to split a block across lines
-
 	sudo_status = event.server.roles.find do |role|
-
 	  role.name == "sudo"
-
 	end
 
 	#check if they *are* Sudo
-
 	if user.role? sudo_status
-		
 		user.remove_role(sudo_status)
 		
 		event << "#{user.mention}@mikubot~$ sudo has been revoked!"
-		
-		return
+	else
+		user.add_role(sudo_status)
 
+		event << "#{user.mention}@mikubot~$ sudo has been activated!"
 	end
-
-	user.add_role(sudo_status)
-
-	event << "#{user.mention}@mikubot~$ sudo has been activated!"
-	
-	event << nil
 end
