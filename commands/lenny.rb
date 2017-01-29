f19d689aa31b57095cac2a75ee7f16842a210325
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 15:46:15 -0500

    Update lenny.rb

--- commands/lenny.rb
+++ commands/lenny.rb
@@ -4,9 +4,9 @@
 $bot.command(
 	:lenny,
 	min_args: 0,
-	description: "Replaces command with a lenny face",
+	description: "Generate a lenny face",
 	usage: "lenny",
 	help_available: true
 ) do | event |
-	event.message.edit("( ° ʖ °)")
+	event << "( ° ʖ °)"
 end
