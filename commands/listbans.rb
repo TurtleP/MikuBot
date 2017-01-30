f76b45d3701d46c9c347800c1b9833368746e466
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 20:00:21 -0500

    Update and rename list_bans.rb to listbans.rb

--- commands/listbans.rb
+++ commands/listbans.rb
@@ -1,8 +1,11 @@
+# listbans.rb
+# list bans on the server
+
 $bot.command(
 	:listbans,
 	min_args: 0,
 	description: "List the users banned on the server.",
-	usage: "list_bans",
+	usage: "listbans",
 	help_available: true
 ) do | event |
 	ban_list = event.server.bans
