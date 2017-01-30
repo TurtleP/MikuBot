eaa1ea6d033be5f732ae9c6c5da0a2e1d5deb4e7
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 20:33:29 -0500

    Update rip.rb

--- commands/rip.rb
+++ commands/rip.rb
@@ -8,5 +8,13 @@ $bot.command(
 	usage: "rip (user)",
 	help_available: true
 ) do | event |
+	user = event.message.mentions.first.on(event.server)
 	
+	msg = "Press **_F_** to pay respects"
+	
+	if user.nil?
+		event << msg
+	else
+		event << msg + " to #{user.username}!"
+	end
 end
