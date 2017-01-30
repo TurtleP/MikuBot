8b44bc0658d09fbb9dbcb6b5057560ee2ccf833f
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-01-29 20:40:22 -0500

    Update rip.rb

--- commands/rip.rb
+++ commands/rip.rb
@@ -7,18 +7,6 @@ $bot.command(
 	description: "Pay respects",
 	usage: "rip (user)",
 	help_available: true
-) do | event, *user |
-    user = user.join(' ')
-
-	user_obj = event.server.bans.find do | user_object |
-		user_object.username == user
-	end
-	
-	msg = "Press **_F_** to pay respects"
-	
-	if user_obj.nil?
-		event << msg
-	else
-		event << msg + " to #{user.username}!"
-	end
+) do | event |
+	event << "Press **_F_** to pay respects"
 end
