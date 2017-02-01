8c1c420bfe5eb2478dec36b8696c8748f75b9037
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2017-02-01 00:31:17 -0500

    Update slap.rb

--- commands/slap.rb
+++ commands/slap.rb
@@ -10,15 +10,11 @@ $bot.command(
 ) do | event |
 	user = event.message.mentions.first.on(event.server)
 
-	if user.nil?
-		event << "Could not find the specified user."
+	if user.username == $bot.username
+		event << "Nice try, #{event.message.author.username}."
+	elseif user.username == event.message.author.username
+		event << "You have slapped yourself. Good job, #{event.message.author.username} :ok_hand:"
 	else
-		if user.username == $bot.username
-			event << "Nice try, #{event.message.author.username}."
-		elseif user.username == event.message.author.username
-			event << "You have slapped yourself. Good job, #{event.message.author.username} :ok_hand:"
-		else
-			event << "#{user.mention} has been slapped by #{event.message.author().username}!"
-		end
+		event << "#{user.mention} has been slapped by #{event.message.author().username}!"
 	end
 end
